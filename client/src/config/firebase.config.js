c02009e3925699ee10fb230dce8d97d9318f00b6
neelmani15 <nileshuitbu@gmail.com>
2024-08-31 17:19:44 +0530

    Added vercel.json successfully

--- client/src/config/firebase.config.js
+++ client/src/config/firebase.config.js
@@ -4,7 +4,7 @@ import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    authDomain: "projectmusicplayer-35db7.firebaseapp.com",
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -12,6 +12,8 @@ const firebaseConfig = {
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+console.log("Firebase Auth Domain:", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+
 const auth = getAuth(app);
 const storage = getStorage(app);
 
