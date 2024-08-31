451424a63fbb45d83bd90f54b704320697ca19c7
neelmani15 <nileshuitbu@gmail.com>
2024-08-31 17:11:23 +0530

    Added vercel.json success

--- client/src/config/firebase.config.js
+++ client/src/config/firebase.config.js
@@ -1,10 +1,7 @@
-// src/config/firebase.config.js
-
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
-// Firebase configuration object
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -14,12 +11,7 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase app
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-console.log("Firebase Auth Domain:", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-
-
-// Initialize Firebase Auth and Storage
 const auth = getAuth(app);
 const storage = getStorage(app);
 
